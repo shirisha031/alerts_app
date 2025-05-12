39490bc948c88b075963f6480076ac8bfd575bb5
shirisha031 <shirisha.m.031@gmail.com>
2025-05-12 17:22:14 +0530

    csrf check

--- student_alerts_app/settings.py
+++ student_alerts_app/settings.py
@@ -26,6 +26,11 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
  
+CSRF_TRUSTED_ORIGINS = [
+    'https://message-alert-hpgmdhewb8c7gfdd.centralindia-01.azurewebsites.net',
+]
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
