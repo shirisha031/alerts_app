8fcb27f8ce7aa420f73a73ce30d5a018ba2d13c8
shirisha031 <shirisha.m.031@gmail.com>
2025-05-12 17:34:43 +0530

    chnages in settings

--- student_alerts_app/settings.py
+++ student_alerts_app/settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
-from dotenv import load_dotenv
-load_dotenv()
-
 import os
 import posixpath
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
